5a71951b99a3ecc124f788854a8df9b64fc4c1ac
Plabick <peterlabick@gmail.com>
2023-03-30 13:23:16 -0400

    hardcode url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE || "https://webdev-tuiter-server.onrender.com/api";
+const API_BASE = "https://webdev-tuiter-server.onrender.com/api/tuits";
 const TUITS_API=`${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
